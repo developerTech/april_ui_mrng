2b294c1b081318bdfade3dcdca4e7d909c5afa1c
aakashdeveloper <ahanda205@gmail.com>
2022-05-17 07:31:39 +0530

    javascript

---
+++ javascript/1datatype.js
@@ -0,0 +1,94 @@
+// EcmaScript
+//es5 
+//es6
+
+String > "Hiii" 'Hello' `Hey` '34346' "true"
+Number > 65 6565766 87868.46537 .5746
+Boolean > true false
+
+//es5 
+var a = "Hii"
+var b = 10
+var c = true
+
+typeof(a)
+'string'
+typeof(b)
+'number'
+typeof(c)
+'boolean'
+var d = "2132"
+typeof(d)
+'string'
+var e = 224.34
+typeof(e)
+'number'
+var e = .34 
+typeof(e)
+'number'
+
+var a = 10
+var b = 20
+a+b (addition)
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+b%a
+0
+4%2
+0
+4%3
+1
+
+2%4
+2
+3%4
+3
+
+var a = "Hi"
+var b = "JavaScript"
+a+b  (concat)
+'HiJavaScript'  
+a-b
+NaN (Not a number)
+b-a
+NaN
+a*b
+NaN
+a/b
+NaN
+
+var b = 10
+undefined
+a+b
+'Hi10'
+a-b
+NaN
+
+String + String = String
+String + Number = String
+Number + String = String
+Number + Number = Number
+
+"10"+20+"30"
+"1020"+"30"
+'102030'
+
+10+20+"30"
+30+"30"
+'3030'
+
+"10"+20+30
+"1020"+30
+'102030'
+
+"10"+20+"30"-1
+102029
+10+20+"30"-1
+3029
+"10"+20+30-1
+102029
