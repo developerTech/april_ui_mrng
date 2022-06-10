4c6e980b42ccaf48a8a49aa889437d11359c55d5
aakashdeveloper <ahanda205@gmail.com>
2022-06-10 07:30:46 +0530

    final

---
+++ javascript/46sets.js
@@ -0,0 +1,17 @@
+let set1 = new Set([10,20,30,40])
+set1
+Set(4) {10, 20, 30, 40}
+let getEntry= set1.entries()
+undefined
+getEntry
+SetIterator {10 => 10, 20 => 20, 30 => 30, 40 => 40}
+getEntry.next().value
+(2) [10, 10]
+getEntry.next().value
+(2) [20, 20]
+getEntry.next().value
+(2) [30, 30]
+getEntry.next().value
+(2) [40, 40]
+getEntry.next().value
+undefined
